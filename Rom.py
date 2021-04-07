894acbd939c252bd28bb7a074465b15903645775
codemann8 <codemann8@gmail.com>
2021-04-07 12:15:26 -0500

    Milestone successful transition to midpoint

--- Rom.py
+++ Rom.py
@@ -27,7 +27,7 @@ from EntranceShuffle import door_addresses, exit_ids
 
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = '5d3546d716515487416bb4cde56d62bf'
+RANDOMIZERBASEHASH = '5c4f3e34c9179f6fea697a01bef48724'
 
 
 class JsonRom(object):
