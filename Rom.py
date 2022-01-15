0bc72f77cf0f4e12764be761131865653b7df168
codemann8 <codemann8@gmail.com>
2022-01-15 13:30:02 -0600

    Restored vanilla ladder at Dark Graveyard in Inverted

--- Rom.py
+++ Rom.py
@@ -33,7 +33,7 @@ from source.classes.SFX import randomize_sfx
 
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = 'c5ca2300535898aeb44355d9ce3689aa'
+RANDOMIZERBASEHASH = '6ae34ddfb50b2e113bcd688715d2e689'
 
 
 class JsonRom(object):
