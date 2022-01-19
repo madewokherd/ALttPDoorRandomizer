a61a3d3693bad8cbdffc6524b6bdfd701a4e8351
codemann8 <codemann8@gmail.com>
2022-01-19 02:31:54 -0600

    Fixed Hera boss music to correctly read boss defeated flag

--- Rom.py
+++ Rom.py
@@ -33,7 +33,7 @@ from source.classes.SFX import randomize_sfx
 
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = '3e62bf77f3b6be521c932b66a9931602'
+RANDOMIZERBASEHASH = 'ea4215dbf86da55ffa1255fa5e687b62'
 
 
 class JsonRom(object):
