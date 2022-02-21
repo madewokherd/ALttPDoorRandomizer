a0d51327b8ee9f3a8f8cc9f5a3ac27000342a8f0
codemann8 <codemann8@gmail.com>
2022-02-21 04:40:13 -0600

    Fix capitalization issue with hints

--- Rom.py
+++ Rom.py
@@ -2304,7 +2304,7 @@ def write_strings(rom, world, player, team):
             this_location = world.find_items_not_key_only(flute_item, player)
             if this_location:
                 this_hint = this_location[0].item.hint_text + ' can be found ' + hint_text(this_location[0]) + '.'
-                this_hint = this_hint.capitalize()
+                this_hint = this_hint[0].upper() + this_hint[1:]
                 tt[hint_locations.pop(0)] = this_hint
             items_to_hint.remove(flute_item)
         if world.keyshuffle[player]:
@@ -2321,7 +2321,7 @@ def write_strings(rom, world, player, team):
             random.shuffle(this_location)
             if this_location:
                 this_hint = this_location[0].item.hint_text + ' can be found ' + hint_text(this_location[0]) + '.'
-                this_hint = this_hint.capitalize()
+                this_hint = this_hint[0].upper() + this_hint[1:]
                 tt[hint_locations.pop(0)] = this_hint
                 hint_count -= 1
 
