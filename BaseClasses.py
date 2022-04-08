72343f8c285af0aefb803003df8ee60f80ec986a
codemann8 <codemann8@gmail.com>
2022-04-08 11:41:11 -0500

    Fixed issue with flute access with Activated flute

--- BaseClasses.py
+++ BaseClasses.py
@@ -1330,7 +1330,7 @@ class CollectionState(object):
         if any(map(lambda i: i.name in ['Ocarina', 'Ocarina (Activated)'], self.world.precollected_items)):
             return True
         lw = self.world.get_region('Kakariko Area', player)
-        return self.has('Ocarina Activated', player) or (self.has('Ocarina', player) and lw.can_reach(self) and self.is_not_bunny(lw, player))
+        return self.has('Ocarina (Activated)', player) or (self.has('Ocarina', player) and lw.can_reach(self) and self.is_not_bunny(lw, player))
 
     def can_melt_things(self, player):
         return self.has('Fire Rod', player) or (self.has('Bombos', player) and self.has_sword(player))
